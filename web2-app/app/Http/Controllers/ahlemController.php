156495b12d20ea1505af93c5e5b4450d9a97ca32
Hammouti Mohamed Amine <m.hammouti.inf@gmail.com>
2020-02-18 11:17:22 +0100

    laravel 5.8

---
+++ web2-app/app/Http/Controllers/ahlemController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ahlemController extends Controller
+{ function A1($name){
+    echo "hello".$name;
+}
+function med(){
+    return view ('mohamed');
+}
+}
+
