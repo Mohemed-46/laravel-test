81c22c8f05bba9963d2ab44bbfeba10a12842ebb
Hammouti Mohamed Amine <m.hammouti.inf@gmail.com>
2020-02-25 09:32:56 +0100

    modifier rout

--- web2-app/routes/web.php
+++ web2-app/routes/web.php
@@ -16,7 +16,7 @@ Route::get('/', function () {
 });
 
 
-Route::get('/med/{name}','ahlemController@A1');
+Route::get('/ahlem/{name}','ahlemController@A1');
 Route::get('/med','ahlemController@med');
 
 
